c6c271b6949e496902727263ee90a0738d5c727f
ECcentricOG <umeshunde003@gmail.com>
2026-06-05 15:51:54 +0530

    day 4 question is added and solved

--- sql/day_4_question.sql
+++ sql/day_4_question.sql
@@ -2,4 +2,15 @@
 -- shows a 3-month rolling avg alongside running total, and flags any month where revenue dropped 20%+ vs the previous month. 
 -- Write the full query.
 
+select
+    order_id
+    ,order_date
+    ,extract(month from order_date) as month
+    ,amount
+    ,sum(amount) over(partition by extract(year from order_date)
+    order by order_date) as running_avg
+    ,avg(amount) over(partition by extract(year from order_date)
+    order by order_date range between '2 month' preceding and current row
+    ) as three_month_rolling_avg
+from orders;
 
