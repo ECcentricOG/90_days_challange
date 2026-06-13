de02f350b697d35614889481b1689d0380f97f63
ECcentricOG <umeshunde003@gmail.com>
2026-06-13 11:57:38 +0530

    Product of array except self is added and leetcode 238 is solved using python

---
+++ dsa/product_of_array_except_self.py
@@ -0,0 +1,18 @@
+from typing import List
+
+
+def product_except_self(nums:List[int]) -> List[int]:
+    n:int = len(nums)
+    left:List[int] = [1] * n
+    right:List[int] = [1] * n
+
+    for i in range(1, n):
+        left[i] = left[i - 1] * nums[i - 1]
+
+    for i in range(n - 2, -1, -1):
+        right[i] = right[i + 1] * nums[i + 1]
+
+    for i in range(n):
+        nums[i] = left[i] * right[i]
+
+    return nums
