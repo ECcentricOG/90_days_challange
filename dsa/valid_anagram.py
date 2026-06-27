347ecf2000d8a5fddf9c5426f61c6b14f237c8e0
ECcentricOG <umeshunde003@gmail.com>
2026-06-27 19:14:52 +0530

    logs are added

--- dsa/valid_anagram.py
+++ dsa/valid_anagram.py
@@ -2,16 +2,16 @@ from typing import Dict
 
 
 def is_anagram(s:str, t:str) -> bool:
-    tracker:dict = {}
+    tracker:Dict = {}
     for ch in s:
         tracker[ch] = tracker.get(ch, 0) + 1
     for ch in t:
         if ch not in tracker:
-            return false
+            return False
         tracker[ch] -= 1
         if tracker[ch] == 0:
             tracker.pop(ch)
 
     if len(tracker) == 0:
-        return true
-    return false
+        return True
+    return False
