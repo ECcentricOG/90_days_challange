19b0fd5fcad6a8445d1131bc1e887fd9bd17a062
ECcentricOG <umeshunde003@gmail.com>
2026-07-07 15:28:56 +0530

    Longest Substring Without Repeating Characters is solved and added leetcode no 3 is solved using 2 pointer and map

--- dsa/longest_subtring_non_repeating.py
+++ dsa/longest_subtring_non_repeating.py
@@ -1,21 +1,11 @@
-from typing import List
-
-
 def lenght_of_longest_substr(s:str) -> int:
-    if len(s) < 2:
-        return 1
+    tracker = {}
     left:int = 0
-    right:int = 1
-    tracker:List = [s[0]]
-    max = 1
-    while right < len(s):
-        if s[right] not in tracker:
-            tracker.append(s[right])
-        if len(tracker) > max:
-            max = len(tracker)
-        if s[right] in tracker:
-            left += 1
-            tracker.pop(0)
-        right += 1
-
-    return max
+    max_len = 0
+    for right in range(len(s)):
+        if s[right] in tracker and tracker[s[right]] >= left:
+            left = tracker[s[right]] + 1
+        else:
+            max_len = max(max_len, right - left + 1)
+        tracker[s[right]] = right
+    return max_len
