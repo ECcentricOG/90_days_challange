4885b615e657e07c7c36fa567838028edea7dabb
ECcentricOG <umeshunde003@gmail.com>
2026-07-11 00:24:42 +0530

    db connections for psql is done

---
+++ python/psql_connection.py
@@ -0,0 +1,51 @@
+from contextlib import contextmanager
+import psycopg2 
+from psycopg2 import OperationalError
+import logging
+
+logging.basicConfig(
+    level=logging.INFO, 
+    format="%(asctime)s | %(levelname)s | %(message)s"
+)
+logger = logging.getLogger(__name__)
+
+class DBConnectionException(Exception):
+    def __init__(self, message) -> None:
+        self.message = message
+        super().__init__(self.message)
+
+class DBQueryError(Exception):
+    def __init__(self, message) -> None:
+        self.message = message
+        super().__init__(self.message)
+
+@contextmanager
+def db_connect():
+    conn = None
+    try:
+        logger.info("Connecting to PSQL DB")
+        conn = psycopg2.connect(
+            host="localhost",
+            database="challange",
+            user="psql",
+            password="psql",
+            port=5432
+        )
+        logger.info("Connected to PSQL DB")
+        yield conn
+
+    except OperationalError as e:
+        logger.error(f"Connection failed: {e}") 
+        raise DBConnectionException(f"Could not connect to PostgreSQL: {e}")
+
+    except Exception as e:
+        logger.exception("Unexpected error while managing DB connection.") 
+        raise DBConnectionException(f"Unexpected DB connection error: {e}")
+
+    finally:
+        if conn is not None:
+            try:
+                conn.close()
+                logger.info("DB Connections closed")
+            except Exception as e:
+                logger.warning(f"Failed to close connection cleanly: {e}")
